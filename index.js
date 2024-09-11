4f6b61caecaf69c5d06560cecbeb7291aa4ca917
João Victor <150389471+jvfsccp@users.noreply.github.com>
2024-09-11 12:34:10 -0300

    aula 1

---
+++ index.js
@@ -0,0 +1,14 @@
+// arrays, objetos
+
+let meta = {
+  value: "ler um livro por mês",
+  checked: false,
+  isChecked: () => {}
+}
+
+console.log()
+meta.isChecked()
+
+// function // arrow function
+
+const criarMeta = () => {}
